d3330f9bf74dfc2bf38cde1f5688e023b33bbf91
Tanner <tannercdolby@gmail.com>
2023-10-14 21:55:45 -0700

    chore: move demo book list

--- src/App.js
+++ src/App.js
@@ -1,6 +1,36 @@
 import Book from './components/Book';
-import { BOOK_LIST } from './open-library/constants';
 import { useState, useEffect } from 'react';
+
+const BOOK_LIST = [
+  {
+    title: "The Hitch Hiker's Guide to the Galaxy",
+    id: 'OL32832269M',
+  },
+  {
+    title: "Harry Potter and the Deathly Hallows",
+    id: 'OL10506200M',
+  },
+  {
+    title: "Star Wars: Splinter of the Mind's Eye",
+    id: 'OL24229002M',
+  },
+  {
+    title: "Star Wars: Heir to the Empire",
+    id: 'OL2028412M',
+  },
+  {
+    title: "Star Trek",
+    id: 'OL24373163M',
+  },
+  {
+    title: "The Hobbit",
+    id: 'OL27289715M',
+  },
+  {
+    title: "The Lion, the Witch and the Wardrobe",
+    id: 'OL7265111M',
+  }
+];
 
 function App() {
   const [book, setBook] = useState(BOOK_LIST[0]);
