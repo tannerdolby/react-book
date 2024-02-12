1eb47cdd5d7eae4c32d54693f67913529fb25f21
Tanner <tannercdolby@gmail.com>
2024-02-11 17:38:16 -0800

    render a list of <Book> components

--- src/App.js
+++ src/App.js
@@ -1,60 +1,10 @@
-import Book from './components/Book';
-import { useState, useEffect } from 'react';
-
-const BOOK_LIST = [
-  {
-    title: "The Hitch Hiker's Guide to the Galaxy",
-    id: 'OL32832269M',
-  },
-  {
-    title: "Harry Potter and the Deathly Hallows",
-    id: 'OL10506200M',
-  },
-  {
-    title: "Star Wars: Splinter of the Mind's Eye",
-    id: 'OL24229002M',
-  },
-  {
-    title: "Star Wars: Heir to the Empire",
-    id: 'OL2028412M',
-  },
-  {
-    title: "Star Trek",
-    id: 'OL24373163M',
-  },
-  {
-    title: "The Hobbit",
-    id: 'OL27289715M',
-  },
-  {
-    title: "The Lion, the Witch and the Wardrobe",
-    id: 'OL7265111M',
-  }
-];
+import BookList from './components/BookList';
 
 function App() {
-  const [book, setBook] = useState(BOOK_LIST[0]);
-  const [index, setIndex] = useState(0);
-
-  useEffect(() => {
-    setBook(BOOK_LIST[index % BOOK_LIST.length]);
-  }, [index]);
-
   return (
     <div className='app'>
       <div className='book-demo'>
-        <button
-          className='next-btn'
-          onClick={(_) => {
-            setIndex((index + 1) % BOOK_LIST.length);
-          }}
-        >
-          View Next Cover
-        </button>
-        <Book
-          title={book.title}
-          openLibraryId={book.id}
-        />
+        <BookList />
       </div>
     </div>
   );
---
+++ src/components/BookList.js
@@ -0,0 +1,49 @@
+import Book from "./Book";
+
+export default function BookList({}) {
+    return (
+        <ul className="book-list">
+          {BOOK_LIST.map((book, i) => {
+            return (
+              <li key={`${book.title}-${i}`}>
+                <Book
+                  title={book.title} 
+                  openLibraryId={book.id}
+                />
+              </li>
+            )
+          })}
+        </ul>
+    );
+}
+
+const BOOK_LIST = [
+    {
+      title: "The Hitch Hiker's Guide to the Galaxy",
+      id: 'OL32832269M',
+    },
+    {
+      title: "Harry Potter and the Deathly Hallows",
+      id: 'OL10506200M',
+    },
+    {
+      title: "Star Wars: Splinter of the Mind's Eye",
+      id: 'OL24229002M',
+    },
+    {
+      title: "Star Wars: Heir to the Empire",
+      id: 'OL2028412M',
+    },
+    {
+      title: "Star Trek",
+      id: 'OL24373163M',
+    },
+    {
+      title: "The Hobbit",
+      id: 'OL27289715M',
+    },
+    {
+      title: "The Lion, the Witch and the Wardrobe",
+      id: 'OL7265111M',
+    }
+];
